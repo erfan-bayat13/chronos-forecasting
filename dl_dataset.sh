668162b19247c37391014e5c819c2c2d9c00a537
non-greek <andrea.grieco@gmx.com>
2025-02-04 14:55:08 +0100

    fixed download script

--- dl_dataset.sh
+++ dl_dataset.sh
@@ -3,6 +3,6 @@ rm -rf ./data
 mkdir ./data
 curl -L -o ./data/stock_market_dataset.zip\
   https://www.kaggle.com/api/v1/datasets/download/jacksoncrow/stock-market-dataset
-unzip ./data/stock_market_dataset.zip
+unzip ./data/stock_market_dataset.zip -d ./data/
 rm stock_market_dataset.zip
 python rw_generation.py --display-first 10 --min-sigma 0.3 --min-initial-price 100 --output-dir ./data/GBM_synth/ --num-series 5000
