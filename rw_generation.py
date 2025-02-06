51a8c8c9edc0b93cb00f5d45b0546b504bbe2460
non-greek <andrea.grieco@gmx.com>
2025-02-06 16:28:01 +0100

    fixed default parameters to match testing and changed parameters from years to business days

--- rw_generation.py
+++ rw_generation.py
@@ -8,9 +8,7 @@ app = typer.Typer(pretty_exceptions_enable=False)
 
 
 # -------- Geometric Brownian motion generation ---------
-def generate_data(num_series, length_series, years, min_initial_price, max_initial_price, min_mu, max_mu, min_sigma, max_sigma):
-    # initialize the step length
-    dt = years/length_series
+def generate_data(num_series, length_series, step_length, min_initial_price, max_initial_price, min_mu, max_mu, min_sigma, max_sigma):
     # drift coefficent
     mu = np.random.uniform(min_mu, max_mu, size=(num_series,))
     # initial stock price
@@ -18,9 +16,9 @@ def generate_data(num_series, length_series, years, min_initial_price, max_initi
     # volatility
     sigma = np.random.uniform(min_sigma, max_sigma, size=(num_series,))
     # array of random noise
-    normal_data = np.random.normal(0, np.sqrt(dt), size=(length_series, num_series))
+    normal_data = np.random.normal(0, np.sqrt(step_length), size=(length_series, num_series))
     # generate all the requested series
-    series = np.exp((mu-sigma**2/2)*dt + sigma * normal_data)
+    series = np.exp((mu-sigma**2/2)*step_length + sigma * normal_data)
     # add one row to insert initial pries(the multiplier must start from one)
     series = np.vstack([np.ones(num_series), series])
     # multiply all columns, so that the series will be on the columns
@@ -40,14 +38,14 @@ def save_series(series, output_dir, filename='brownian_motions.arrow'):
 
     
 @app.command()
-def main(num_series:int= 1000,
+def main(num_series:int= 5000,
          length_series:int=1024,
-         years:float=0.5,
+         use_business_days:bool=True, #how many days should pass to compute a year? 260 -> business days
          min_initial_price:float=0,
-         max_initial_price:float=300,
+         max_initial_price:float=2000,
          min_mu:float=-0.10,
          max_mu:float=0.10,
-         min_sigma:float=0.2,
+         min_sigma:float=0.01,
          max_sigma:float=1,
          output_dir:str = './output/',
          display_first:int = 0,
@@ -58,15 +56,19 @@ def main(num_series:int= 1000,
         np.random.seed(seed)
     assert display_first < num_series
 
+    days_in_year = 260 if use_business_days else 365
+    years = length_series/days_in_year
+
     series = generate_data(num_series=num_series,
                            length_series=length_series,
-                           years=years,
+                           step_length=1/days_in_year, # step length to compute one unit (in years)
                            min_initial_price=min_initial_price,
                            max_initial_price=max_initial_price,
                            min_mu=min_mu,
                            max_mu=max_mu,
                            min_sigma=min_sigma,
                            max_sigma=max_sigma)
+
     time = np.linspace(0,years, length_series+1)
     if display_first > 0:
         for i in range(display_first):
